f4a247f2073767350f73a41f50047f5f4488ce0e
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2021-11-04 21:02:44 -0300

    cambio a hostname y puerto

--- send/send.go
+++ send/send.go
@@ -13,7 +13,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@10.6.40.194:50010/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
